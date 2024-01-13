e2152c0796ad7440649d3d1d3ecfd6e8e885d1bb
BIMAL SHARMA <105968871+BimalKanxa@users.noreply.github.com>
2024-01-13 10:19:09 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = 'mongodb://localhost:27017/evernotedb'
+const mongoURI = 'mongodb+srv://sharmabimal:sharmabimal@cluster0.trycpuj.mongodb.net/?retryWrites=true&w=majority'
 
 const connectToMongo = async () => {
     try {
